2d37956de7672d1ba9ec55805948664c6ccaab78
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-09 17:08:13 +0200

    Update settings.js for Abdallah

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;TR0mnYbB#VxqMxYuaELL0fAZAxLcaZpM3w0LR4r201jyZop98CrU",
+  OWNER_NUMBER: "254780464373", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
