de0facc5bb842003a25956357546363432fcc55d
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-09 18:22:23 +0200

    Update settings.js for Honey

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;aYkhwbJZ#MbXMAZGVmxgaM3bG37XWO9RV7P4OIoamKbF_Gx1AzSw",
+  OWNER_NUMBER: "242068359512", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
