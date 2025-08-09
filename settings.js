8523a149a379d7228b3932d202b5e60ba70fee87
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-09 17:01:22 +0200

    Update settings.js for Sireid

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;zU1QjIJS#54Agnu6WZMP7_xT7dyGdywPn1cKENAgQM6ngKitOcJA",
+  OWNER_NUMBER: "254780464373", 
+  PREFIX: "#"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
