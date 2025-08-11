74475724a2217b5efd25cf9369ef56c6e6dfedfb
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-11 09:43:06 +0200

    Update settings.js for crazy

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;6JF0yQLT#utG94dGw5Ame-HHWeeajR0r_ftQtZXbVUNbJOr89ZPw",
+  OWNER_NUMBER: "237620114013", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
