4704e12a0de041cd23699f3705771efa1d571212
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-11 11:32:22 +0200

    Update settings.js with new variables for shazam

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
+  OWNER_NUMBER: "263784262759",
   SESSION_ID: "SUBZERO-MD;;;qBkAHRhT#W1Fxqit8wkz9mSIEjjf6pfRMAkARPGYnpufRDLdb4YQ",
-  OWNER_NUMBER: "263784262759", 
-  PREFIX: "."
+  PREFIX: ".",
 };
