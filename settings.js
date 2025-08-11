d76c3513adf3e3f880eb7bcfbcaa7afd0e71c97e
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-11 10:16:37 +0200

    Update settings.js for Zanele01

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;OUESyK6T#zP3cdLglpcF_-ZyKTVXRQCBHXZoClPb48zrhvuAcElk",
+  OWNER_NUMBER: "263716729222", 
+  PREFIX: "+"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
