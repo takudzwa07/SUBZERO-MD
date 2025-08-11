b9173fbc54fefaa12667b21c7a83505250c90343
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-11 11:27:29 +0200

    Update settings.js for shazam

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;qBkAHRhT#W1Fxqit8wkz9mSIEjjf6pfRMAkARPGYnpufRDLdb4YQ",
+  OWNER_NUMBER: "263784262759", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
