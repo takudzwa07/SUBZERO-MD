99ba1c97b09203a2d306bcb927f23136314194c7
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-11 15:31:52 +0200

    Update settings.js for Tinosa1

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;mcUxkYxS#rYZuO_42moTjESwNO7HHLKeF9aovC4-zyeNv6Hu9ONA",
+  OWNER_NUMBER: "+263719584260", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
